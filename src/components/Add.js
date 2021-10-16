699ff79ced37391a072329887338a2f230d1340e
javad boroji <javadboroji2222>
2021-10-16 20:17:18 +0330

    versin1

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,59 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import CartMovie from './CartMovie';
+import {GlobalProvider} from '../context/GlobalState'
+import {InputGroup,Button,FormControl,Container,Row,Col} from 'react-bootstrap'
 function Add() {
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState([])
+    const inputHandler = e=>{
+        e.preventDefault()
+        setInput(e.target.value)
+        
+          fetch(`https://imdb-api.com/en/API/SearchMovie/k_muuzob39/${e.target.value}`)
+    .then(res=>res.json())
+    .then((data)=> {
+        console.log(data);
+        if(!data.errors){
+        setResult(data.results)
+        }else{
+        setResult([])
+        }
+        } )
+    }
+    
     return (
-        <div>
-            Add    
-        </div>
+        <GlobalProvider>
+        <Container>
+            <Row>
+                <Col md={12}>
+                <InputGroup className="mb-3">
+                    <FormControl className="mt-3"
+                    aria-label="Example text with button addon"
+                    aria-describedby="basic-addon1"
+                    placeholder="search movie"
+                    value={input}
+                    onChange={inputHandler}
+                    />
+             </InputGroup> 
+                </Col>
+       
+            </Row>
+            
+              
+                
+                { (<ul> <Row> {result.map((res)=> 
+                <Col md={6}>
+                    <li key={res.id}> <CartMovie movie={res}/></li>
+                </Col>)} </Row> </ul>)
+                
+                }
+                        
+                   
+              
+            
+    
+        </Container>
+        </GlobalProvider>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { Container, Row ,Nav,Navbar,Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 function Header() {
+    const width={width:'40%'}
     return (
         <header>
-            <Container>
+            <Container fluid >
                 <Row>
                    
-                    <Navbar bg="dark" variant="dark">
-                        <Container>
+                    <Navbar bg="dark" variant="dark" className="d-flex justify-content-between pr-3 pl-3">
+                        
                            <Link to="/">whatchList</Link>
-                            <Nav className=" d-flex  justify-content-around">
-                                <Link to="/"> whatchList</Link>
-                                <Link to="/whatched"> whatched</Link>
+
+                            <Nav className="d-flex justify-content-around" style={width}>
+                                <Link className="ml-2 pt-2" to="/"> whatchList</Link>
+                                <Link className="ml-3 mr-3 pt-2" to="/whatched"> whatched</Link>
                                 <Link to="/add">   <Button variant="primary">Add</Button>{' '}</Link>
                             </Nav>
-                        </Container>
+                       
                     </Navbar>
 
                 </Row>
