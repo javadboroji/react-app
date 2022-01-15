4eef545d090b744e59b0f3a734ca55a68de81cfa
javad boroji <javadboroji2222>
2022-01-15 20:36:19 +0330

    commit react shopp cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React,{useState,useContext} from 'react'
+import { Container ,Row} from 'react-bootstrap'
 import './App.css'
-import Main from './Components/Main'
-import Quize from './Components/Quize'
-import EndScreen from './Components/EndScreen'
-import { QuizeContext } from './Helper/Context'
+
 
 export default function App() {
 
-const [gameState, setGameState] = useState('menu')
-const [score, setScore] = useState(0)
 
   return (
     <div className="app">
-      <h1 className='text-white'> Quize App</h1>
-      <QuizeContext.Provider value={{gameState,setGameState,score,setScore}}>
-      {gameState==="menu"&&<div> <Main /></div>}
-      {gameState==="Quize"&&<div> <Quize /></div>}
-      {gameState==="EndScreen"&&<div> <EndScreen /></div>}
-      </QuizeContext.Provider>
-
+ 
+    <Container fluid>
+      <Row>
+        <header className='p-3'> <a href='#'> React shopp Cart</a></header>
+        <main> product list</main>
+        <footer className='p-3 text-center'> <p>All right reservered</p></footer>
+      </Row>
+    </Container>
       
 
     </div>
